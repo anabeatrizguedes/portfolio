83d5cbccc34070e69c7b2ae2b6dfc64cb610a5c4
Ana <abcavet@gmail.com>
2024-07-24 23:27:08 -0300

    header e about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import Header from './components/Header';
+import AboutSection from './components/AboutSection';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Header />
+      <AboutSection />
     </div>
   );
 }
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import AboutImg from '../assets/images/about-img.jpg';
+import ViewMyWorkBtn from './ViewMyWorkBtn';
+
+function AboutSection() {
+    return (
+        <div className='bg-dark text-light py-5' id='about'>
+            <div className='container'>
+                <div className='flex-column-reverse flex-md-row row'>
+                    <div className='col-md-6 mt-md-0 d-flex flex-column justify-content-center align-items-center'>
+                        <img className='img-fluid w-75 shadow' src={AboutImg} alt="about img" />
+                    </div>
+                    <div className='col-md-6 d-flex flex-column justify-content-center'>
+                        <h2 class='mb-5'>About me</h2>
+
+                        <p className='text-start'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                        <p className='text-start mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                        <ViewMyWorkBtn />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutSection;
