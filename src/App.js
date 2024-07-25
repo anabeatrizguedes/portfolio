9ba0c1a377062b5aee53ac84c03139af4e2104ed
Ana <abcavet@gmail.com>
2024-07-25 00:32:52 -0300

    projects e contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import AboutSection from './components/AboutSection';
+import ProjectsSection from './components/ProjectsSection';
+import ContactSection from './components/ContactSection';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <NavigationBar />
       <Header />
       <AboutSection />
+      <ProjectsSection />
+      <ContactSection />
     </div>
   );
 }
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProjectsSection() {
+    return (
+        <div className='py-5' id='projects'>
+            <div className='container'>
+                <h2 className='mb-5'>My projects</h2>
+
+                <p className='text-align-center'>Coming soon 😉</p>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectsSection;
