74885eba5be39c2a6b0472a7b30300fe7ef50412
Ana <abcavet@gmail.com>
2026-06-17 20:51:40 -0300

    v02

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-function App() {
+function App () {
   return (
-    <>
-      
-    </>
-  )
+    <div className='min-h-screen overflow-x-hidden'>
+      <Navbar />
+      <main></main>
+    </div>
+  );
 }
 
-export default App
+export default App;
