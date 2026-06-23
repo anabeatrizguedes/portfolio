ca6fe22db0ced8f0aacbac44c71a31fca750dc4d
Ana <abcavet@gmail.com>
2026-06-22 21:11:23 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-function App () {
+import Navbar from './layout/Navbar';
+import Hero from '@/sections/Hero';
+import About from '@/sections/About';
+import Projects from '@/sections/Projects';
+import Experience from '@/sections/Experience';
+import Testimonials from '@/sections/Testimonials';
+import Contact from '@/sections/Contact';
+// import Footer from './layout/Footer';
+
+function App() {
   return (
-    <div className='min-h-screen overflow-x-hidden'>
+    <div className="min-h-screen overflow-x-hidden">
       <Navbar />
-      <main></main>
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main>
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,29 @@
+const navLinks = [
+    {href: '#about', label: 'About'},
+    {href: '#projects', label: 'Projects'},
+    {href: '#experience', label: 'Experience'},
+    {href: '#testimonials', label: 'Testimonials'},
+]
+
 export const Navbar = () => {
-    return <header></header>;
-}
+    return (
+    <header className='fixed top-0 left-0 right-0 bg-transparent py-5'>
+        <nav className='container mx-auto px-6 flex items-center justify-between'>
+            <a href='#' className='text-xl font-bold tracking-tight'>
+                PM<span>.</span>
+            </a>
+
+            {/* Desktop nav */}
+            <div>
+                <div>
+                    {navLinks.map((link, index) => (
+                        <a href={link.href} key={index}>
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </nav>
+    </header>
+    );
+};
